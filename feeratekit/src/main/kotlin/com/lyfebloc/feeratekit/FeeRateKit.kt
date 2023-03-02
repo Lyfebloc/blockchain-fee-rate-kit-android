769759b269afe6d2378a95920eff1b4b299978cb
JustinM <107500172+Lyfeloop@users.noreply.github.com>
2023-03-01 21:04:13 -0500

    Initial commit

---
+++ feeratekit/src/main/kotlin/com/lyfebloc/feeratekit/FeeRateKit.kt
@@ -0,0 +1,38 @@
+package com.lyfebloc.feeratekit
+
+import com.lyfebloc.feeratekit.model.FeeProviderConfig
+import com.lyfebloc.feeratekit.providers.BtcCoreProvider
+import com.lyfebloc.feeratekit.providers.EvmProvider
+import io.reactivex.Single
+import java.math.BigInteger
+
+class FeeRateKit(providerConfig: FeeProviderConfig) {
+
+    private val btcProvider = BtcCoreProvider(providerConfig)
+    private val ethProvider = EvmProvider(providerConfig.ethEvmUrl, providerConfig.ethEvmAuth)
+    private val bscProvider = EvmProvider(providerConfig.bscEvmUrl)
+
+    fun bitcoin(blockCount: Int): Single<BigInteger> {
+        return btcProvider.getFeeRate(blockCount)
+    }
+
+    fun litecoin(): Single<BigInteger> {
+        return Single.just(BigInteger("1"))
+    }
+
+    fun bitcoinCash(): Single<BigInteger> {
+        return Single.just(BigInteger("3"))
+    }
+
+    fun dash(): Single<BigInteger> {
+        return Single.just(BigInteger("1"))
+    }
+
+    fun ethereum(): Single<BigInteger> {
+        return ethProvider.getFeeRate()
+    }
+
+    fun binanceSmartChain(): Single<BigInteger> {
+       return bscProvider.getFeeRate()
+    }
+}
